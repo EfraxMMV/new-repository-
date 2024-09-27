d12fd9725e5a4f830364f5e6a2b062f542b5dc54
Efrain Arias <earias7@ucol.mx>
2024-09-27 23:30:50 +0000

    Add initial calculator pages and modify devcontainer settings

---
+++ pages/Suma.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+
+def sumar(a: float, b: float) -> str:
+    a = float(a)
+    b = float(b)
+    resultado = a+b
+    return (f"El resultado es: {resultado}")
+
+with st.container():
+    st.write("---")
+    st.header("Suma de dos numeros")
+    a1 = st.text_input("Ingresa el primer valor para sumar: ")
+    b2 = st.text_input("Ingresa el segundo valor para sumar: ")
+
+    if st.button("Calcular"):
+        resf = sumar(a1, b2)
+        st.write(resf)
+        
