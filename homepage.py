69e409cb843ed56531c3ee2aa8973a675a770680
Efrain Arias <earias7@ucol.mx>
2024-09-28 17:30:37 +0000

    Añadido archivo informacion_Personal.py and Multiplicar.py

--- homepage.py
+++ homepage.py
@@ -3,4 +3,8 @@ import streamlit as st
 st.title("Tablero Interactivo")
 st.header("Pagina web simple donde los usuarios pueden ingresar datos y las funciones resuelven")
 
-
+with st.container():
+    st.write("---")
+    st.header("¿Quienes somos?")
+    st.write("Se trata de una pagina web, sonde los usuarios ingresan cantidades y/o datos, para que la misma pagina logre desempeñar sus funciones programadas y hacer calculos con los datos ingresados por el usuario")
+    
